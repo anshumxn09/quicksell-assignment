8e8ce5bb3601dcc79777a94cc033e5f9ec0a6b20
Anshuman <sharmaaanshumaan@gmail.com>
2023-08-23 19:18:25 +0530

    Main Container for Rendering Cards

---
+++ src/components/DashBoard/DashView.jsx
@@ -0,0 +1,68 @@
+import React from "react";
+import { useSelector } from "react-redux";
+import { DiCodeigniter } from "react-icons/di";
+import { AiOutlinePlus } from "react-icons/ai";
+import "./DashView.css";
+import Card from "../Card/Card";
+
+const DashView = () => {
+  const { selectedData, user } = useSelector(
+    (state) => state.SelectDataReducer
+  );
+
+  return (
+    selectedData && (
+      <div className="dashContainer" style={{ justifyContent: "space-evenly" }}>
+        {selectedData.map((elem, index) => {
+          return (
+            <>
+              <div key={index} className="dashCardContainer">
+                <div className="dashCardHeading flex-sb">
+                  <div className="leftView">
+                    {!user ? (
+                      <DiCodeigniter />
+                    ) : (
+                      <>
+                        <div
+                          className="imageContainer relative"
+                          style={{ width: "15px", height: "15px", display : 'inline-block' }}
+                        >
+                          <img
+                            style={{
+                              width: "100%",
+                              height: "100%",
+                              borderRadius: "50%",
+                            }}
+                            src="https://images.unsplash.com/photo-1633332755192-727a05c4013d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8dXNlcnxlbnwwfHwwfHx8MA%3D%3D&w=1000&q=80"
+                            alt="UserImage"
+                          />
+                        </div>
+                      </>
+                    )}
+                    <span>
+                      {" "}
+                      {elem[index]?.title} {elem[index]?.value?.length}
+                    </span>
+                  </div>
+                  <div className="rightView">
+                    <AiOutlinePlus />{" "}
+                    <span style={{ letterSpacing: "2px" }}>...</span>
+                  </div>
+                </div>
+                <div className="dashList flex-gap-10">
+                  {elem[index]?.value?.map((elem, ind) => {
+                    return (
+                      <Card id={elem.id} title={elem.title} tag={elem.tag} />
+                    );
+                  })}
+                </div>
+              </div>
+            </>
+          );
+        })}
+      </div>
+    )
+  );
+};
+
+export default DashView;
