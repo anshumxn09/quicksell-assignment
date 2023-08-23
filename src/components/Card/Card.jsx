28a8190ef3927c7a653adb344262d9a6ed2532f0
Anshuman <sharmaaanshumaan@gmail.com>
2023-08-23 19:18:08 +0530

    App Card UI

---
+++ src/components/Card/Card.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import './Card.css';
+
+const Card = ({id, title, tag, status}) => {
+  return (
+    <div className="cardContainer flex-gap-10" style={{gap : '5px'}}>
+        <div className="cardHeading flex-sb">
+            <span style={{textTransform : "uppercase"}} className='color-grey'>{id}</span>
+            <div className="imageContainer relative" style={{ width : "30px", height : "30px"}}>
+                <img style={{width : "100%", height : "100%",  borderRadius : "50%" }}  src="https://images.unsplash.com/photo-1633332755192-727a05c4013d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8dXNlcnxlbnwwfHwwfHx8MA%3D%3D&w=1000&q=80" alt="UserImage" />
+                <div className="showStatus"></div>
+            </div>
+        </div>
+        <div className="cardTitle" style={{fontWeight : 200}} >
+            <p>{title}</p>
+        </div>
+        <div className="cardTags">
+        <div className="tags color-grey"> ... </div>
+            {
+                tag?.map((elem, index) => {
+                    return <div key={index} className="tags color-grey"> <span>•</span> {elem}</div>
+                })
+            }
+        </div>
+    </div>
+  )
+}
+
+export default Card;
